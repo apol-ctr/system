1e76ceaf002c975c7aa9e15d982044508ab1145e
vitorcape <95989293+vitorcape@users.noreply.github.com>
2023-02-15 11:13:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/* MENU */
+$(".header_toggle").click(function () {
+    if($("body").hasClass("body-pd")){
+        $("#body-pd").removeClass("body-pd");
+        $("header").removeClass("body-pd");
+        $("#nav-bar").removeClass("show");
+        $("#header-toggle").removeClass("bx-x");
+    }
+    else{
+        $("body").addClass("body-pd");
+        $("header").addClass("body-pd");
+        $("#nav-bar").addClass("show");
+        $("#header-toggle").addClass("bx-x");
+    };
+ });
+
+/* LINK MENU*/
 for (var i = 0; i < document.links.length; i++) {
     if (document.links[i].href == document.URL) {
         document.links[i].className = 'nav_link active';
